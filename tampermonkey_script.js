926001267a03db226b3b2459008d64caeca97604
Gerardufoin <gerardufoin2@hotmail.com>
2015-11-27 21:25:04 +0100

    Update author; Add check for frames

--- tampermonkey_script.js
+++ tampermonkey_script.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Mangafox sometimes doesn't load pages. In these cases, the page is loaded via jax and displayed. Also allows to go to next page on clicking on "big" images.
-// @author       Flagoul
+// @author       Flagoul && Gerardufoin
 // @match        http://mangafox.me/*
 // @grant        none
 // @require      file:///PATH_TO_MU
