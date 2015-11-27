11e6057e1705110bf71006b0f88fbc115ada3500
Gerardufoin <gerardufoin2@hotmail.com>
2015-11-27 21:01:56 +0100

    Mangafox-upgrade.js is require in tampermonkey_script.js

--- mangafox-upgrade.js
+++ mangafox-upgrade.js
@@ -1,12 +1,3 @@
-// ==UserScript==
-// @name         Mangafox upgrade
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Mangafox sometimes doesn't load pages. In these cases, the page is loaded via jax and displayed. Also allows to go to next page on clicking on "big" images.
-// @author       Flagoul
-// @match        http://mangafox.me/*
-// @grant        none
-// ==/UserScript==
 /* jshint -W097 */
 'use strict';
 
---
+++ tampermonkey_script.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Mangafox upgrade
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Mangafox sometimes doesn't load pages. In these cases, the page is loaded via jax and displayed. Also allows to go to next page on clicking on "big" images.
+// @author       Flagoul
+// @match        http://mangafox.me/*
+// @grant        none
+// @require      file:///PATH_TO_MU
+// ==/UserScript==
