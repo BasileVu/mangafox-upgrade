9300b77dddf35811c585a50e82ca3b18c7f4b39e
Gerardufoin <gerardufoin2@hotmail.com>
2015-11-29 12:48:13 +0100

    v0.2

--- mangafox-upgrade.js
+++ mangafox-upgrade.js
@@ -1,3 +1,15 @@
+// ==UserScript==
+// @name         Mangafox upgrade
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  Mangafox sometimes doesn't load pages. In these cases, the page is loaded via jax and displayed. Also allows to go to next page on clicking on "big" images.
+// @author       Flagoul && Gerardufoin
+// @match        http://mangafox.me/*
+// @grant        none
+// @run-at       document-start
+// @require      https://code.jquery.com/jquery-2.1.4.min.js
+// ==/UserScript==
+
 /* jshint -W097 */
 'use strict';
 
